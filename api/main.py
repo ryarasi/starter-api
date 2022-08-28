3363ef48014ee1addace633214d8e759edb83c25
Ragav Yarasi <ragav.code@gmail.com>
2022-08-28 20:42:12 +0530

    Initiated a starter api

---
+++ api/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+
+app = FastAPI()
+
+port = int(os.getenv('PORT', 8000))
+reload = bool(os.getenv('RELOAD', 1))
+host=str(os.getenv('HOST','0.0.0.0'))
+
+@app.get('/')
+def Home():
+    return "Service is Running..."
+
+
+if __name__ == "__main__":
+    print('port => ', port)
+    print('reload => ', reload)
+    print('host => ', host)
+    uvicorn.run('main:app', port=port, reload=reload, host=host)
